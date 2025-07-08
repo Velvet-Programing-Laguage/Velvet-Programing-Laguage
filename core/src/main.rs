6212e0626142a1ae77e9007e81069cba8393a4d3
Michal92299 <michal.99967@gmail.com>
2025-07-08 11:40:48 +0200

    Update main.rs

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,11 +1,12 @@
 use velvet_core::{parse_velvet, Interpreter};
 use std::env;
+use std::fs;
 
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
     let file = args.get(1).expect("Provide a .vel file");
-    let code = std::fs::read_to_string(file).expect("Failed to read file");
+    let code = fs::read_to_string(file).expect("Failed to read file");
     let ast = parse_velvet(&code).expect("Parsing failed");
     let mut interpreter = Interpreter::new();
     let gui_data = interpreter.execute(ast);
