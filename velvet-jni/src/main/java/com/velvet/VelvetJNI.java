0613e3f2eb75443bd2e7f26517969a411828feca
Michal92299 <michal.99967@gmail.com>
2025-07-08 18:05:41 +0200

    Create VelvetJNI.java

---
+++ velvet-jni/src/main/java/com/velvet/VelvetJNI.java
@@ -0,0 +1,13 @@
+package com.velvet;
+
+public class VelvetJNI {
+    static {
+        System.loadLibrary("velvet_core");
+    }
+
+    public native void initJNI();
+
+    public native String runPygame(String args);
+
+    public native String runTk(String args);
+}
