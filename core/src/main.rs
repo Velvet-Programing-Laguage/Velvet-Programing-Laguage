66a9b6b3bd94e6bdd6e2384897d4f248a6bede60
VoidArc Studio <chudybyczek2137@gmail.com>
2025-07-08 09:56:10 +0200

    Create main.rs

---
+++ core/src/main.rs
@@ -0,0 +1,13 @@
+use velvet_core::{parse_velvet, Interpreter};
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file = args.get(1).expect("Provide a .vel file");
+    let code = std::fs::read_to_string(file).expect("Failed to read file");
+    let ast = parse_velvet(&code).expect("Parsing failed");
+    let mut interpreter = Interpreter::new();
+    let gui_data = interpreter.execute(ast);
+    println!("{}", gui_data);
+}
