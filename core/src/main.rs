e634f8faf04c301ae3b780ef6195539445b653ef
Michal92299 <michal.99967@gmail.com>
2025-07-08 17:43:42 +0200

    Update main.rs

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,6 +1,24 @@
 use velvet_core::{parse_velvet, Interpreter};
 use std::env;
 use std::fs;
+use jni::JNIEnv;
+use jni::objects::{JClass, JString};
+use jni::sys::jstring;
+
+#[no_mangle]
+pub extern "system" fn Java_com_velvet_VelvetJNI_initJNI(env: JNIEnv, _class: JClass) {
+    println!("JNI initialized for Velvet");
+}
+
+#[no_mangle]
+pub extern "system" fn Java_com_velvet_VelvetJNI_runPygame(_env: JNIEnv, _class: JClass, _args: JString) -> jstring {
+    _env.new_string("Pygame initialized via JNI").unwrap().into_inner()
+}
+
+#[no_mangle]
+pub extern "system" fn Java_com_velvet_VelvetJNI_runTk(_env: JNIEnv, _class: JClass, _args: JString) -> jstring {
+    _env.new_string("Tk initialized via JNI").unwrap().into_inner()
+}
 
 #[tokio::main]
 async fn main() {
