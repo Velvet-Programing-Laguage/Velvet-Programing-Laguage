283f9b84eb27dd3364a3fe4eac0a763ebe753d03
Michal92299 <michal.99967@gmail.com>
2025-07-08 11:39:24 +0200

    Update parser.rs

--- core/src/parser.rs
+++ core/src/parser.rs
@@ -1,10 +1,9 @@
 use pest::Parser;
 use pest_derive::Parser;
-use serde_json::{json, Value};
 use std::error::Error;
 use std::fmt;
 
-#[derive]
+#[derive(Parser)]
 #[grammar = "velvet.pest"]
 pub struct VelvetParser;
 
@@ -13,6 +12,9 @@ pub enum Node {
     Say(Box<Expr>),
     Set(String, Box<Expr>),
     If(Box<Expr>, Vec<Node>, Option<Vec<Node>>),
+    For(String, Box<Expr>, Vec<Node>),
+    Function(String, Vec<String>, Vec<Node>),
+    Import(String),
     Window(Vec<WindowProp>),
 }
 
@@ -21,6 +23,7 @@ pub enum WindowProp {
     Title(String),
     Size(u32, u32),
     Button(String, Vec<Node>),
+    TextInput(String, String), // Added: Text input with id and placeholder
 }
 
 #[derive(Debug, Clone)]
@@ -29,6 +32,7 @@ pub enum Expr {
     Number(f64),
     Ident(String),
     Arith(Box<Expr>, String, Box<Expr>),
+    Call(String, Vec<Expr>), // Added: Function call
 }
 
 #[derive(Debug)]
@@ -82,6 +86,38 @@ pub fn parse_velvet(code: &str) -> Result<Vec<Node>, Box<dyn Error>> {
                     .transpose()?;
                 nodes.push(Node::If(Box::new(condition), then_branch, else_branch));
             }
+            Rule::for_stmt => {
+                let mut inner = pair.into_inner();
+                let var = inner.next().unwrap().as_str().to_string();
+                let range = parse_expr(inner.next().unwrap())?;
+                let body = inner
+                    .filter(|p| p.as_rule() != Rule::DEDENT)
+                    .map(parse_statement)
+                    .collect::<Result<Vec<_>, _>>()?;
+                nodes.push(Node::For(var, Box::new(range), body));
+            }
+            Rule::function => {
+                let mut inner = pair.into_inner();
+                let name = inner.next().unwrap().as_str().to_string();
+                let params = inner
+                    .next()
+                    .map(|p| {
+                        p.into_inner()
+                            .filter(|p| p.as_rule() == Rule::IDENT)
+                            .map(|p| p.as_str().to_string())
+                            .collect::<Vec<_>>()
+                    })
+                    .unwrap_or_default();
+                let body = inner
+                    .filter(|p| p.as_rule() != Rule::DEDENT)
+                    .map(parse_statement)
+                    .collect::<Result<Vec<_>, _>>()?;
+                nodes.push(Node::Function(name, params, body));
+            }
+            Rule::import => {
+                let module = pair.into_inner().next().unwrap().as_str().trim_matches('"').to_string();
+                nodes.push(Node::Import(module));
+            }
             Rule::window => {
                 let mut props = Vec::new();
                 for prop in pair.into_inner().filter(|p| p.as_rule() != Rule::DEDENT) {
@@ -104,6 +140,12 @@ pub fn parse_velvet(code: &str) -> Result<Vec<Node>, Box<dyn Error>> {
                                 .map(parse_statement)
                                 .collect::<Result<Vec<_>, _>>()?;
                             props.push(WindowProp::Button(text, actions));
+                        }
+                        Rule::textinput => {
+                            let mut inner = prop.into_inner();
+                            let id = inner.next().unwrap().as_str().trim_matches('"').to_string();
+                            let placeholder = inner.next().unwrap().as_str().trim_matches('"').to_string();
+                            props.push(WindowProp::TextInput(id, placeholder));
                         }
                         _ => {}
                     }
@@ -128,6 +170,15 @@ fn parse_expr(pair: pest::iterators::Pair<Rule>) -> Result<Expr, Box<dyn Error>>
             let right = parse_expr(inner.next().unwrap())?;
             Ok(Expr::Arith(Box::new(left), op, Box::new(right)))
         }
+        Rule::call => {
+            let mut inner = pair.into_inner();
+            let name = inner.next().unwrap().as_str().to_string();
+            let args = inner
+                .filter(|p| p.as_rule() != Rule::EOI)
+                .map(parse_expr)
+                .collect::<Result<Vec<_>, _>>()?;
+            Ok(Expr::Call(name, args))
+        }
         _ => Err(Box::new(ParseError {
             message: format!("Invalid expression: {}", pair.as_str()),
         })),
@@ -146,8 +197,36 @@ fn parse_statement(pair: pest::iterators::Pair<Rule>) -> Result<Node, Box<dyn Er
             let expr = parse_expr(inner.next().unwrap())?;
             Ok(Node::Set(ident, Box::new(expr)))
         }
+        Rule::for_stmt => {
+            let mut inner = pair.into_inner();
+            let var = inner.next().unwrap().as_str().to_string();
+            let range = parse_expr(inner.next().unwrap())?;
+            let body = inner
+                .filter(|p| p.as_rule() != Rule::DEDENT)
+                .map(parse_statement)
+                .collect::<Result<Vec<_>, _>>()?;
+            Ok(Node::For(var, Box::new(range), body))
+        }
+        Rule::function => {
+            let mut inner = pair.into_inner();
+            let name = inner.next().unwrap().as_str().to_string();
+            let params = inner
+                .next()
+                .map(|p| {
+                    p.into_inner()
+                        .filter(|p| p.as_rule() == Rule::IDENT)
+                        .map(|p| p.as_str().to_string())
+                        .collect::<Vec<_>>()
+                })
+                .unwrap_or_default();
+            let body = inner
+                .filter(|p| p.as_rule() != Rule::DEDENT)
+                .map(parse_statement)
+                .collect::<Result<Vec<_>, _>>()?;
+            Ok(Node::Function(name, params, body))
+        }
         _ => Err(Box::new(ParseError {
             message: format!("Invalid statement: {}", pair.as_str()),
         })),
     }
-                              }
+}
