5b007cd9e3c11fd759cdea3a09e82885f8e1a6b2
Michal92299 <michal.99967@gmail.com>
2025-07-08 16:43:55 +0200

    Update lib.rs

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -2,6 +2,6 @@ pub mod parser;
 pub mod interpreter;
 pub mod stdlib;
 
-pub use parser::{Node, WindowProp, Expr};
+pub use parser::{Node, WindowProp, Expr, Type};
 pub use interpreter::Interpreter;
 pub use stdlib::register_stdlib;
