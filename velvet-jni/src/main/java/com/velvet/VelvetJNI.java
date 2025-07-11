660bb0526fa964d1a56f3d2578fc7695a0dfad7c
Michal92299 <michal.99967@gmail.com>
2025-07-11 09:06:39 +0200

    Update VelvetJNI.java

--- velvet-jni/src/main/java/com/velvet/VelvetJNI.java
+++ velvet-jni/src/main/java/com/velvet/VelvetJNI.java
@@ -14,4 +14,24 @@ public class VelvetJNI {
     public native String captureCamera(String args);
 
     public native String createPDF(String args);
+
+    public native String base64Encode(String args);
+
+    public native String compressData(String args);
+
+    public native String hashMD5(String args);
+
+    public native String httpPost(String args);
+
+    public native String imageCrop(String args);
+
+    public native String jsonValidate(String args);
+
+    public native String xmlParse(String args);
+
+    public native String yamlValidate(String args);
+
+    public native String dbMongoConnect(String args);
+
+    public native String dbRedisConnect(String args);
 }
