02252bffbfefbac022fb7adf83702aaba0ff5212
Michal92299 <michal.99967@gmail.com>
2025-07-11 11:58:21 +0200

    Create modules.rs

---
+++ core/src/modules.rs
@@ -0,0 +1,27 @@
+pub fn python_requests(args: &str) -> String {
+    format!("Python requests executed: {}", args)
+}
+
+pub fn cpp_boost(args: &str) -> String {
+    format!("C++ Boost executed: {}", args)
+}
+
+pub fn csharp_json(args: &str) -> String {
+    format!("C# JSON executed: {}", args)
+}
+
+pub fn ruby_httparty(args: &str) -> String {
+    format!("Ruby HTTParty executed: {}", args)
+}
+
+pub fn js_axios(args: &str) -> String {
+    format!("JS Axios executed: {}", args)
+}
+
+pub fn rust_flate2(args: &str) -> String {
+    format!("Rust flate2 executed: {}", args)
+}
+
+pub fn java_jython(args: &str) -> String {
+    format!("Java Jython executed: {}", args)
+}
