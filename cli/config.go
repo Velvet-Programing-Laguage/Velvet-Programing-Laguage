e18681b7f557afc5374b7a8011fd65e1c07f6624
Michal92299 <michal.99967@gmail.com>
2025-07-11 12:11:39 +0200

    Update config.go

--- cli/config.go
+++ cli/config.go
@@ -6,17 +6,22 @@ import (
 )
 
 type Config struct {
-    Dependencies map[string]string
+    Debug        bool              `json:"debug"`
+    Dependencies map[string]string `json:"dependencies"`
 }
 
-func loadConfig() (*Config, error) {
+func loadConfig() Config {
     file, err := os.Open("vel.json")
     if err != nil {
-        return nil, err
+        logger.Println("Error loading config:", err)
+        return Config{}
     }
     defer file.Close()
     var config Config
     decoder := json.NewDecoder(file)
-    err = decoder.Decode(&config)
-    return &config, err
+    if err := decoder.Decode(&config); err != nil {
+        logger.Println("Error decoding config:", err)
+        return Config{}
+    }
+    return config
 }
