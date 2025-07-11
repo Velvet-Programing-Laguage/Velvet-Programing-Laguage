062596852b2ec72b0889f2577646e26fe23c3c52
Michal92299 <michal.99967@gmail.com>
2025-07-11 21:36:10 +0200

    Update ast.rs

--- src/core/ast.rs
+++ src/core/ast.rs
@@ -14,6 +14,9 @@ pub enum Statement {
     Say(Expr),
     Let(String, Option<String>, Option<Expr>),
     Const(String, Option<Expr>),
+    If(Expr, Vec<Statement>, Option<Vec<Statement>>),
+    For(String, Expr, Vec<Statement>),
+    Fun(String, Vec<(String, Option<String>)>, Option<String>, Vec<Statement>),
     Expr(Expr),
-    // Add other statement types (if, for, fun, etc.) as needed
+    // Add other statement types as needed
 }
