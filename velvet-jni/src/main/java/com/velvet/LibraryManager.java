e9b00089fab8c7de9d419b5d9c147204548dea59
Michal92299 <michal.99967@gmail.com>
2025-07-11 16:04:27 +0200

    Update LibraryManager.java

--- velvet-jni/src/main/java/com/velvet/LibraryManager.java
+++ velvet-jni/src/main/java/com/velvet/LibraryManager.java
@@ -1,42 +1,49 @@
 package com.velvet;
 
-import org.python.util.PythonInterpreter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LibraryManager {
-    private PythonInterpreter pythonInterpreter;
-    private ObjectMapper jsonMapper;
-    private Map<String, LibraryAdapter> adapters;
+    private final VelvetJNI jni;
+    private final ObjectMapper mapper;
+    private final Map<String, ModuleAdapter> adapters;
 
     public LibraryManager() {
-        pythonInterpreter = new PythonInterpreter();
-        jsonMapper = new ObjectMapper();
-        adapters = new HashMap<>();
+        this.jni = new VelvetJNI();
+        this.mapper = new ObjectMapper();
+        this.adapters = new HashMap<>();
+        registerAdapters();
+    }
+
+    private void registerAdapters() {
         adapters.put("python_requests", new PythonRequestsAdapter());
         adapters.put("cpp_boost", new CppBoostAdapter());
         adapters.put("csharp_json", new CsharpJsonAdapter());
         adapters.put("ruby_httparty", new RubyHttpartyAdapter());
         adapters.put("js_axios", new JsAxiosAdapter());
         adapters.put("rust_flate2", new RustFlate2Adapter());
-        adapters.put("java_jython", new JavaJythonAdapter(pythonInterpreter));
+        adapters.put("java_jython", new JavaJythonAdapter());
+        adapters.put("tauri_gui", new TauriGuiAdapter());
+        adapters.put("wayland_gui", new WaylandGuiAdapter());
+        adapters.put("ai_tensorflow", new AiTensorflowAdapter());
+        adapters.put("ai_pytorch", new AiPytorchAdapter());
+        adapters.put("perf_parallel", new PerfParallelAdapter());
+        adapters.put("perf_crypto", new PerfCryptoAdapter());
+        adapters.put("db_sqlite", new DbSqliteAdapter());
+        adapters.put("net_websocket", new NetWebsocketAdapter());
+        adapters.put("gpu_cuda", new GpuCudaAdapter());
     }
 
-    public String handleAction(String action) {
-        try {
-            String[] parts = action.split(",", 2);
-            String library = parts[0];
-            String command = parts.length > 1 ? parts[1] : "";
-            LibraryAdapter adapter = adapters.get(library);
-            if (adapter == null) return "Unknown library: " + library;
-            return adapter.execute(command);
-        } catch (Exception e) {
-            return "Error handling action: " + e.getMessage();
+    public String execute(String module, String args) throws Exception {
+        ModuleAdapter adapter = adapters.get(module);
+        if (adapter == null) {
+            throw new Exception("Module not found: " + module);
         }
+        return adapter.execute(args);
     }
-}
 
-interface LibraryAdapter {
-    String execute(String command);
+    public String executeAsync(String module, String args) throws Exception {
+        return jni.asyncExec(module, args);
+    }
 }
