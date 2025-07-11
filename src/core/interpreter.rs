4103bc94675853f1c50e4a7e5ff43ba999cea80d
Michal92299 <michal.99967@gmail.com>
2025-07-11 21:37:14 +0200

    Update interpreter.rs

--- src/core/interpreter.rs
+++ src/core/interpreter.rs
@@ -3,37 +3,72 @@ use std::collections::HashMap;
 
 pub struct VelvetInterpreter {
     variables: HashMap<String, Expr>,
+    functions: HashMap<String, (Vec<(String, Option<String>)>, Option<String>, Vec<Statement>)>,
 }
 
 impl VelvetInterpreter {
     pub fn new() -> Self {
         VelvetInterpreter {
             variables: HashMap::new(),
+            functions: HashMap::new(),
         }
     }
 
     pub fn interpret(&mut self, ast: Vec<Statement>) -> Result<(), String> {
         for stmt in ast {
-            match stmt {
-                Statement::Say(expr) => {
-                    let value = self.eval_expr(&expr)?;
-                    println!("Output: {}", self.expr_to_string(&value));
+            self.execute_stmt(&stmt)?;
+        }
+        Ok(())
+    }
+
+    fn execute_stmt(&mut self, stmt: &Statement) -> Result<(), String> {
+        match stmt {
+            Statement::Say(expr) => {
+                let value = self.eval_expr(expr)?;
+                println!("Output: {}", self.expr_to_string(&value));
+            }
+            Statement::Let(ident, _ty, expr) => {
+                if let Some(e) = expr {
+                    let value = self.eval_expr(e)?;
+                    self.variables.insert(ident.clone(), value);
                 }
-                Statement::Let(ident, _ty, expr) => {
-                    if let Some(e) = expr {
-                        let value = self.eval_expr(&e)?;
-                        self.variables.insert(ident, value);
+            }
+            Statement::Const(ident, expr) => {
+                if let Some(e) = expr {
+                    let value = self.eval_expr(e)?;
+                    self.variables.insert(ident.clone(), value);
+                }
+            }
+            Statement::If(condition, then_block, else_block) => {
+                let cond_val = self.eval_expr(condition)?;
+                if self.is_truthy(&cond_val) {
+                    for stmt in then_block {
+                        self.execute_stmt(stmt)?;
+                    }
+                } else if let Some(else_block) = else_block {
+                    for stmt in else_block {
+                        self.execute_stmt(stmt)?;
                     }
                 }
-                Statement::Const(ident, expr) => {
-                    if let Some(e) = expr {
-                        let value = self.eval_expr(&e)?;
-                        self.variables.insert(ident, value);
+            }
+            Statement::For(ident, expr, block) => {
+                let list = self.eval_expr(expr)?;
+                if let Expr::List(items) = list {
+                    for item in items {
+                        self.variables.insert(ident.clone(), item);
+                        for stmt in block {
+                            self.execute_stmt(stmt)?;
+                        }
                     }
+                } else {
+                    return Err("For loop expects a list".to_string());
                 }
-                Statement::Expr(expr) => {
-                    self.eval_expr(&expr)?;
-                }
+            }
+            Statement::Fun(ident, params, ret_type, block) => {
+                self.functions.insert(ident.clone(), (params.clone(), ret_type.clone(), block.clone()));
+            }
+            Statement::Expr(expr) => {
+                self.eval_expr(expr)?;
             }
         }
         Ok(())
@@ -57,8 +92,18 @@ impl VelvetInterpreter {
                         (Expr::Number(l), Expr::Number(r)) => Ok(Expr::Number(l - r)),
                         _ => Err("Invalid types for -".to_string()),
                     },
+                    "*" => match (left_val, right_val) {
+                        (Expr::Number(l), Expr::Number(r)) => Ok(Expr::Number(l * r)),
+                        _ => Err("Invalid types for *".to_string()),
+                    },
+                    "/" => match (left_val, right_val) {
+                        (Expr::Number(l), Expr::Number(r)) => Ok(Expr::Number(l / r)),
+                        _ => Err("Invalid types for /".to_string()),
+                    },
                     "==" => Ok(Expr::Number(if left_val == right_val { 1.0 } else { 0.0 })),
                     "|>" => Ok(right_val), // Simplified pipe: pass right value
+                    "and" => Ok(Expr::Number(if self.is_truthy(&left_val) && self.is_truthy(&right_val) { 1.0 } else { 0.0 })),
+                    "or" => Ok(Expr::Number(if self.is_truthy(&left_val) || self.is_truthy(&right_val) { 1.0 } else { 0.0 })),
                     _ => Err(format!("Unsupported operator: {}", op)),
                 }
             }
@@ -76,8 +121,39 @@ impl VelvetInterpreter {
                     _ => Err(format!("Unsupported unary operator: {}", op)),
                 }
             }
-            Expr::Call(_ident, _args) => Err("Function calls not implemented".to_string()),
-            Expr::List(_items) => Err("Lists not implemented".to_string()),
+            Expr::Call(ident, args) => {
+                if let Some((params, _ret_type, block)) = self.functions.get(ident) {
+                    let mut local_vars = HashMap::new();
+                    for (i, (param_name, _)) in params.iter().enumerate() {
+                        if i < args.len() {
+                            let arg_val = self.eval_expr(&args[i])?;
+                            local_vars.insert(param_name.clone(), arg_val);
+                        }
+                    }
+                    let mut local_interpreter = VelvetInterpreter {
+                        variables: local_vars,
+                        functions: self.functions.clone(),
+                    };
+                    for stmt in block {
+                        local_interpreter.execute_stmt(stmt)?;
+                    }
+                    Ok(Expr::Number(0.0)) // Placeholder return
+                } else {
+                    Err(format!("Undefined function: {}", ident))
+                }
+            }
+            Expr::List(items) => {
+                let evaluated = items.iter().map(|i| self.eval_expr(i)).collect::<Result<Vec<_>, _>>()?;
+                Ok(Expr::List(evaluated))
+            }
+        }
+    }
+
+    fn is_truthy(&self, expr: &Expr) -> bool {
+        match expr {
+            Expr::Number(n) => *n != 0.0,
+            Expr::String(s) => !s.is_empty(),
+            _ => true,
         }
     }
 
