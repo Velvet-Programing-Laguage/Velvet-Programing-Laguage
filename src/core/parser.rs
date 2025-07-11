06b932af3e5c4998d9a92838681f168179074e68
Michal92299 <michal.99967@gmail.com>
2025-07-11 21:36:37 +0200

    Update parser.rs

--- src/core/parser.rs
+++ src/core/parser.rs
@@ -26,7 +26,7 @@ fn parse_statement(pair: pest::iterators::Pair<Rule>) -> Statement {
         Rule::let => {
             let mut inner = pair.into_inner();
             let ident = inner.next().unwrap().as_str().to_string();
-            let ty = inner.next().map(|t| t.as_str().to_string());
+            let ty = inner.next().map(|t| t.into_inner().next().unwrap().as_str().to_string());
             let expr = inner.next().map(parse_expr);
             Statement::Let(ident, ty, expr)
         }
@@ -36,12 +36,51 @@ fn parse_statement(pair: pest::iterators::Pair<Rule>) -> Statement {
             let expr = inner.next().map(parse_expr);
             Statement::Const(ident, expr)
         }
+        Rule::if_stmt => {
+            let mut inner = pair.into_inner();
+            let condition = parse_expr(inner.next().unwrap());
+            let then_block = parse_statement_block(inner.next().unwrap());
+            let else_block = inner.next().map(parse_statement_block);
+            Statement::If(condition, then_block, else_block)
+        }
+        Rule::for_stmt => {
+            let mut inner = pair.into_inner();
+            let ident = inner.next().unwrap().as_str().to_string();
+            let expr = parse_expr(inner.next().unwrap());
+            let block = parse_statement_block(inner.next().unwrap());
+            Statement::For(ident, expr, block)
+        }
+        Rule::fun => {
+            let mut inner = pair.into_inner();
+            let ident = inner.next().unwrap().as_str().to_string();
+            let params = inner.next().map(|p| {
+                p.into_inner()
+                    .filter(|pair| pair.as_rule() == Rule::fn_param)
+                    .map(|param| {
+                        let mut param_inner = param.into_inner();
+                        let param_name = param_inner.next().unwrap().as_str().to_string();
+                        let param_type = param_inner.next().map(|t| t.as_str().to_string());
+                        (param_name, param_type)
+                    })
+                    .collect()
+            }).unwrap_or_default();
+            let ret_type = inner.next().map(|t| t.as_str().to_string());
+            let block = parse_statement_block(inner.next().unwrap());
+            Statement::Fun(ident, params, ret_type, block)
+        }
         Rule::expr_stmt => {
             let expr = pair.into_inner().next().unwrap();
             Statement::Expr(parse_expr(expr))
         }
         _ => unimplemented!("Statement type not supported: {:?}", pair.as_rule()),
     }
+}
+
+fn parse_statement_block(pair: pest::iterators::Pair<Rule>) -> Vec<Statement> {
+    pair.into_inner()
+        .filter(|p| p.as_rule() == Rule::statement)
+        .map(parse_statement)
+        .collect()
 }
 
 fn parse_expr(pair: pest::iterators::Pair<Rule>) -> Expr {
@@ -55,17 +94,7 @@ fn parse_expr(pair: pest::iterators::Pair<Rule>) -> Expr {
             }
             expr
         }
-        Rule::logic => {
-            let mut inner = pair.into_inner();
-            let mut expr = parse_expr(inner.next().unwrap());
-            for op_pair in inner {
-                let op = op_pair.as_str().to_string();
-                let right = parse_expr(op_pair.into_inner().next().unwrap());
-                expr = Expr::Binary(op, Box::new(expr), Box::new(right));
-            }
-            expr
-        }
-        Rule::equality | Rule::comparison | Rule::term | Rule::factor => {
+        Rule::logic | Rule::equality | Rule::comparison | Rule::term | Rule::factor => {
             let mut inner = pair.into_inner();
             let mut expr = parse_expr(inner.next().unwrap());
             for op_pair in inner {
@@ -77,9 +106,14 @@ fn parse_expr(pair: pest::iterators::Pair<Rule>) -> Expr {
         }
         Rule::unary => {
             let mut inner = pair.into_inner();
-            let op = inner.next().unwrap().as_str().to_string();
-            let expr = parse_expr(inner.next().unwrap());
-            Expr::Unary(op, Box::new(expr))
+            let first = inner.next().unwrap();
+            if first.as_rule() == Rule::unary {
+                let op = first.as_str().to_string();
+                let expr = parse_expr(inner.next().unwrap());
+                Expr::Unary(op, Box::new(expr))
+            } else {
+                parse_expr(first)
+            }
         }
         Rule::primary => {
             let inner = pair.into_inner().next().unwrap();
@@ -103,4 +137,4 @@ fn parse_expr(pair: pest::iterators::Pair<Rule>) -> Expr {
         }
         _ => unimplemented!("Expression type not supported: {:?}", pair.as_rule()),
     }
-                            }
+}
