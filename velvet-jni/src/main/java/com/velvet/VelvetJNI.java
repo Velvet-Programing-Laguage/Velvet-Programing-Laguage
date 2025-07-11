4074f9c2c4a1572ffdee6cf530c6c484836b10fb
Michal92299 <michal.99967@gmail.com>
2025-07-11 11:51:33 +0200

    Update VelvetJNI.java

--- velvet-jni/src/main/java/com/velvet/VelvetJNI.java
+++ velvet-jni/src/main/java/com/velvet/VelvetJNI.java
@@ -5,27 +5,12 @@ public class VelvetJNI {
         System.loadLibrary("velvet_core");
     }
 
-    public native void initJNI();
-
-    public native String runPygame(String args);
-
-    public native String runTk(String args);
-
-    public native String captureCamera(String args);
-
-    public native String createPDF(String args);
-
+    public native String init(String args);
     public native String pythonRequests(String args);
-
     public native String cppBoost(String args);
-
     public native String csharpJson(String args);
-
     public native String rubyHttparty(String args);
-
     public native String jsAxios(String args);
-
     public native String rustFlate2(String args);
-
     public native String javaJython(String args);
 }
