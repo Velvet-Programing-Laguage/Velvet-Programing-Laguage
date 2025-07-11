c9281c0b83e85c0b0787e7f254fb8f5b54d6719e
Michal92299 <michal.99967@gmail.com>
2025-07-11 16:12:57 +0200

    Update WaylandGuiAdapter.java

--- velvet-jni/src/main/java/com/velvet/WaylandGuiAdapter.java
+++ velvet-jni/src/main/java/com/velvet/WaylandGuiAdapter.java
@@ -1,1 +1,18 @@
+package com.velvet;
 
+public class WaylandGuiAdapter implements ModuleAdapter {
+    private final VelvetJNI jni;
+
+    public WaylandGuiAdapter() {
+        this.jni = new VelvetJNI();
+    }
+
+    @Override
+    public String execute(String args) {
+        try {
+            return jni.waylandGui(args);
+        } catch (Throwable e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+}
