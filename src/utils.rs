4526ca9efe6cd9c88ea360dc2ef52bb34dfee9b2
Michal92299 <michal.99967@gmail.com>
2025-07-18 15:12:24 +0200

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -2,11 +2,11 @@ use std::fs;
 use std::process::Command;
 
 pub fn read_file(path: &str) -> Result<String, String> {
-    fs::read_to_string(path).map_err(|e| format!("Failed to read {}: {}", path, e))
+    fs::read_to_string(path).map_err(|e| format!("Cannot read {}: {}", path, e))
 }
 
 pub fn write_file(path: &str, content: &str) -> Result<(), String> {
-    fs::write(path, content).map_err(|e| format!("Failed to write {}: {}", path, e))
+    fs::write(path, content).map_err(|e| format!("Cannot write {}: {}", path, e))
 }
 
 pub fn run_python_script(script: &str, args: &[&str]) -> Result<std::process::Output, String> {
@@ -14,5 +14,5 @@ pub fn run_python_script(script: &str, args: &[&str]) -> Result<std::process::Ou
         .arg(script)
         .args(args)
         .output()
-        .map_err(|e| format!("Failed to run python script: {}", e))
+        .map_err(|e| format!("Python script error: {}", e))
 }
