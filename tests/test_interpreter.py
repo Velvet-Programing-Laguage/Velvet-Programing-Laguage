1d3b2d86784c40e2f64ec90c01285be5417f46ee
Michal92299 <michal.99967@gmail.com>
2025-07-18 14:59:56 +0200

    Delete tests directory

--- tests/test_interpreter.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from src.hooks.interpreter import execute_velvet
-
-class TestVelvetInterpreter(unittest.TestCase):
-    def test_execute(self):
-        execute_velvet("tests/test_velvet.vel")
-        # Add assertions based on output
