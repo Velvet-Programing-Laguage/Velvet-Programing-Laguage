0931c9ae724fbe0a2b077aa3de375c1a8f642d42
Michal92299 <michal.99967@gmail.com>
2025-07-18 15:01:50 +0200

    Create ast.rs

---
+++ src/ast.rs
@@ -0,0 +1,30 @@
+#[derive(Debug, Clone)]
+pub enum Statement {
+    Say(Expr),
+    Val(String, Option<Expr>, Option<String>),
+    Const(String, Expr, Option<String>),
+    Fun(String, Vec<(String, String)>, Vec<Statement>),
+    If(Expr, Vec<Statement>, Option<Vec<Statement>>),
+    For(String, Expr, Vec<Statement>),
+    While(Expr, Vec<Statement>),
+    Break,
+    Continue,
+    Try(String, Vec<Statement>, Vec<Statement>),
+    Match(Expr, Vec<(String, Vec<Statement>)>),
+    Expr(Expr),
+    Return(Expr),
+    Import(String),
+    Test(String, Vec<Statement>),
+}
+
+#[derive(Debug, Clone)]
+pub enum Expr {
+    String(String),
+    Number(f64),
+    Bool(bool),
+    Ident(String),
+    Binary(Box<Expr>, String, Box<Expr>),
+    Unary(String, Box<Expr>),
+    Call(String, Vec<Expr>),
+    List(Vec<Expr>),
+}
