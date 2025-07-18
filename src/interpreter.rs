d50ef80f8f818f53f82f336e403bfa2ffed45ba9
Michal92299 <michal.99967@gmail.com>
2025-07-18 15:13:17 +0200

    Update interpreter.rs

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -5,14 +5,14 @@ use std::collections::HashMap;
 use std::path::Path;
 
 pub fn run(statements: Vec<Statement>, debug: bool) -> Result<(), String> {
-    let mut env = HashMap::new();
+    let mut env = HashMap::with_capacity(statements.len());
     execute_block(&statements, &mut env, debug)?;
     Ok(())
 }
 
 fn execute_stmt(stmt: &Statement, env: &mut HashMap<String, Value>, debug: bool) -> Result<(), String> {
     if debug {
-        cli::debug(&format!("Executing: {:?}", stmt));
+        cli::debug(&format!("Stmt: {:?}", stmt));
     }
     match stmt {
         Statement::Say(expr) => {
@@ -20,32 +20,26 @@ fn execute_stmt(stmt: &Statement, env: &mut HashMap<String, Value>, debug: bool)
             Ok(())
         }
         Statement::Val(ident, expr, type_anno) => {
-            let value = if let Some(e) = expr {
-                let val = eval_expr(e, env, debug)?;
-                check_type(&val, type_anno)?;
-                val
-            } else {
-                Value::None
-            };
+            let value = expr.as_ref().map_or(Value::None, |e| eval_expr(e, env, debug).unwrap());
+            check_type(&value, type_anno)?;
             env.insert(ident.clone(), value);
             Ok(())
         }
         Statement::Const(ident, expr, type_anno) => {
             if env.contains_key(ident) {
-                return Err(format!("Constant '{}' already defined", ident));
+                return Err(format!("Const '{}' redefinition", ident));
             }
-            let val = eval_expr(expr, env, debug)?;
-            check_type(&val, type_anno)?;
-            env.insert(ident.clone(), val);
+            let value = eval_expr(expr, env, debug)?;
+            check_type(&value, type_anno)?;
+            env.insert(ident.clone(), value);
             Ok(())
         }
-        Statement::Fun(name, params, body) => {
-            env.insert(name.clone(), Value::Function(params.clone(), body.clone()));
+        Statement::Fun(name, params, ret_type, body) => {
+            env.insert(name.clone(), Value::Function(params.clone(), ret_type.clone(), body.clone()));
             Ok(())
         }
         Statement::If(condition, then_block, else_block) => {
-            let cond = eval_expr(condition, env, debug)?;
-            if cond.as_bool()? {
+            if eval_expr(condition, env, debug)?.as_bool()? {
                 execute_block(then_block, env, debug)?;
             } else if let Some(else_block) = else_block {
                 execute_block(else_block, env, debug)?;
@@ -53,8 +47,7 @@ fn execute_stmt(stmt: &Statement, env: &mut HashMap<String, Value>, debug: bool)
             Ok(())
         }
         Statement::For(ident, expr, body) => {
-            let iterable = eval_expr(expr, env, debug)?;
-            for value in iterable.as_list()? {
+            for value in eval_expr(expr, env, debug)?.as_list()? {
                 env.insert(ident.clone(), value);
                 match execute_block(body, env, debug) {
                     Err(e) if e == "Break" => break,
@@ -102,24 +95,20 @@ fn execute_stmt(stmt: &Statement, env: &mut HashMap<String, Value>, debug: bool)
             eval_expr(expr, env, debug)?;
             Ok(())
         }
-        Statement::Return(expr) => {
-            let value = eval_expr(expr, env, debug)?;
-            Err(format!("Return: {}", value))
-        }
-        Statement::Import(module) => {
+        Statement::Return(expr) => Err(format!("Return: {}", eval_expr(expr, env, debug)?)),
+        Statement::Import(module, source) => {
             let module_path = format!("{}.velvet", module);
             if !Path::new(&module_path).exists() {
                 return Err(format!("Module '{}' not found", module));
             }
-            let source = crate::utils::read_file(&module_path)
-                .map_err(|_| format!("Failed to load module '{}'", module))?;
-            let ast = crate::parser::parse(&source)?;
+            let module_source = crate::utils::read_file(&module_path)?;
+            let ast = crate::parser::parse(&module_source)?;
             execute_block(&ast, env, debug)?;
             Ok(())
         }
         Statement::Test(name, body) => {
             if debug {
-                cli::info(&format!("Running test: {}", name));
+                cli::info(&format!("Test: {}", name));
             }
             execute_block(body, &mut env.clone(), debug)?;
             Ok(())
@@ -136,13 +125,13 @@ fn execute_block(stmts: &[Statement], env: &mut HashMap<String, Value>, debug: b
 
 fn eval_expr(expr: &Expr, env: &HashMap<String, Value>, debug: bool) -> Result<Value, String> {
     if debug {
-        cli::debug(&format!("Evaluating: {:?}", expr));
+        cli::debug(&format!("Expr: {:?}", expr));
     }
     match expr {
         Expr::String(s) => Ok(Value::String(s.clone())),
         Expr::Number(n) => Ok(Value::Number(*n)),
         Expr::Bool(b) => Ok(Value::Bool(*b)),
-        Expr::Ident(id) => env.get(id).cloned().ok_or(format!("Undefined variable '{}'", id)),
+        Expr::Ident(id) => env.get(id).cloned().ok_or(format!("Var '{}' not found", id)),
         Expr::Binary(left, op, right) => {
             let left_val = eval_expr(left, env, debug)?;
             let right_val = eval_expr(right, env, debug)?;
@@ -165,7 +154,7 @@ fn eval_expr(expr: &Expr, env: &HashMap<String, Value>, debug: bool) -> Result<V
                 "<=" => Ok(Value::Bool(left_val.as_number()? <= right_val.as_number()?)),
                 "and" => Ok(Value::Bool(left_val.as_bool()? && right_val.as_bool()?)),
                 "or" => Ok(Value::Bool(left_val.as_bool()? || right_val.as_bool()?)),
-                _ => Err(format!("Unsupported operator '{}'", op)),
+                _ => Err(format!("Unknown operator '{}'", op)),
             }
         }
         Expr::Unary(op, expr) => {
@@ -173,31 +162,35 @@ fn eval_expr(expr: &Expr, env: &HashMap<String, Value>, debug: bool) -> Result<V
             match op.as_str() {
                 "-" => Ok(Value::Number(-value.as_number()?)),
                 "!" => Ok(Value::Bool(!value.as_bool()?)),
-                _ => Err(format!("Unsupported unary operator '{}'", op)),
+                _ => Err(format!("Unknown unary op '{}'", op)),
             }
         }
         Expr::Call(name, args) => {
-            let func = env.get(name).ok_or(format!("Undefined function '{}'", name))?;
-            if let Value::Function(params, body) = func {
-                let mut local_env = env.clone();
+            let func = env.get(name).ok_or(format!("Function '{}' not found", name))?;
+            if let Value::Function(params, ret_type, body) = func {
                 if params.len() != args.len() {
-                    return Err(format!("Expected {} arguments, got {}", params.len(), args.len()));
+                    return Err(format!("Expected {} args, got {}", params.len(), args.len()));
                 }
+                let mut local_env = env.clone();
                 for ((param, _), arg) in params.iter().zip(args.iter()) {
                     local_env.insert(param.clone(), eval_expr(arg, env, debug)?);
                 }
-                execute_block(body, &mut local_env, debug)?;
+                let result = execute_block(body, &mut local_env, debug)?;
+                if let Some(ret_type) = ret_type {
+                    if ret_type != "void" {
+                        return Err("Missing return value".to_string());
+                    }
+                }
                 Ok(Value::None)
             } else {
                 Err(format!("'{}' is not a function", name))
             }
         }
-        Expr::List(elements) => {
-            let mut values = Vec::new();
-            for elem in elements {
-                values.push(eval_expr(elem, env, debug)?);
-            }
-            Ok(Value::List(values))
+        Expr::List(elements) => Ok(Value::List(elements.iter().map(|e| eval_expr(e, env, debug)).collect::<Result<_, _>>()?)),
+        Expr::Index(ident, index) => {
+            let list = env.get(ident).ok_or(format!("Var '{}' not found", ident))?.as_list()?;
+            let idx = eval_expr(index, env, debug)?.as_number()? as usize;
+            list.get(idx).cloned().ok_or(format!("Index {} out of bounds", idx))
         }
     }
 }
@@ -205,11 +198,8 @@ fn eval_expr(expr: &Expr, env: &HashMap<String, Value>, debug: bool) -> Result<V
 fn check_type(value: &Value, type_anno: &Option<String>) -> Result<(), String> {
     if let Some(type_anno) = type_anno {
         match (type_anno.as_str(), value) {
-            ("str", Value::String(_)) => Ok(()),
-            ("f64", Value::Number(_)) => Ok(()),
-            ("bool", Value::Bool(_)) => Ok(()),
-            ("list", Value::List(_)) => Ok(()),
-            _ => Err(format!("Type mismatch: expected {}, got {}", type_anno, value)),
+            ("str", Value::String(_)) | ("f64", Value::Number(_)) | ("bool", Value::Bool(_)) | ("list", Value::List(_)) | ("fn", Value::Function(_, _, _)) => Ok(()),
+            _ => Err(format!("Expected {}, got {}", type_anno, value)),
         }
     } else {
         Ok(())
